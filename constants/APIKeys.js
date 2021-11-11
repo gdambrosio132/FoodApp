df5e336b18deb436f3f6fec0a62c04a08aa2b455
Jasur <jasur.shukurov29@gmail.com>
2021-11-11 18:39:13 -0500

    Unit test

--- constants/APIKeys.js
+++ constants/APIKeys.js
@@ -1,3 +1,3 @@
-export const EDAMAM_APP_ID = "45463de4";     // Get API ID from here https://developer.edamam.com/
-export const EDAMAM_APP_KEY = "18bde60eaa6696cf5e2613c6abfc5f02";  // Get API KEY from here https://developer.edamam.com/
-export const CLARIFAI_APP_KEY = "e7a93632be524193a7a68a93023bc793" // Clarifai API KEY 
+export const EDAMAM_APP_ID = "";     // Get API ID from here https://developer.edamam.com/
+export const EDAMAM_APP_KEY = "";  // Get API KEY from here https://developer.edamam.com/
+export const CLARIFAI_APP_KEY = "" // Clarifai API KEY 
